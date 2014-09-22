7f33d6f03899a726886c220cd2e77d7d9f2892cf
Joel Low <joel@joelsplace.sg>
2014-09-22 16:07:34 +0800

    Add the first kind of node we will support - classes.

---
+++ lib/Phortress/AbstractNode.php
@@ -0,0 +1,10 @@
+<?php
+namespace Phortress;
+
+/**
+ * Represents a node in the program.
+ *
+ * @package Phortress
+ */
+class AbstractNode {
+} 
---
+++ lib/Phortress/Node/Class_.php
@@ -0,0 +1,16 @@
+<?php
+namespace Phortress\Node;
+
+/**
+ * Represents a class definition.
+ *
+ * @package Phortress\Node
+ */
+class Class_ extends \Phortress\Symbol {
+	/**
+	 * The parser node for the current class.
+	 *
+	 * @var \PhpParser\Node\Stmt\Class_
+	 */
+	private $node;
+} 
