b1ee263f6ac4c4c7068f4041fe154e629cb373c6
Naomi Leow <naomileowwx@gmail.com>
2014-10-20 00:57:04 +0800

    with arrays containing names of function taking in user input or input from file/database

---
+++ lib/Phortress/Dephenses/DephensesConstants.php
@@ -0,0 +1,81 @@
+<?php
+namespace Phortress\Dephenses;
+/* *
+ * Contains the predefined PHP variables and function names
+ */
+
+$USER_INPUT_GLOBALS = array(
+    '_GET',
+    '_POST',
+    '_COOKIE',
+    '_REQUEST',
+    '_FILES',
+    '_SERVER',
+    '_ENV',
+    'argv' //argc should be safe? since it's just the number of arguments
+);
+
+$FILE_READ_FUNCTIONS = array(
+    'fread', 
+    'fopen', 
+    'popen',
+    'fgets', 
+    'fgetss', //fgetss strips HTML tags
+    'fscanf',
+    'file',
+    'ftell',
+    'file_get_contents',
+    'get_meta_tags',
+    'bzread', //bzip2 file read
+    'gzread', //gz file read
+    'zip_read',
+    'read_exif_data',
+    'exif_read_data', //read EXIF headers from JPEG or TIFF
+    'exif_imagetype', //reads the first bytes of an image and checks its signature
+    'exif_thumbnail', //reads embedded thumbnail of TIFFor JPEG
+    'readdir', //Should be ok, unless server is compromised
+    'scandir',
+    );
+
+$FILE_WRITE_FUNCTIONS = array(
+    'fwrite',
+    'fpassthru',
+    'rewind'
+);
+
+$DATABASE_READ_FUNCTIONS = array(
+    'mysql_fetch_array',
+    'mysql_fetch_row',
+    'mysql_fetch_assoc',
+    'mysql_data_seek',
+    'mysql_query',
+    'mysql_fetch_object',
+    //the above functions are DEPRECATED as of PHP5.5
+    
+    //mysqli
+    'mysqli_fetch_assoc',
+    'mysqli_fetch_row',
+    'mysqli_fetch_object',
+    'mysqli_query',
+    'mysqli_data_seek',
+    'mysqli_fetch_array',
+    
+    //postgre
+    'pg_fetch_all',
+    'pg_fetch_array',
+    'pg_fetch_assoc',
+    'pg_fetch_object',
+    'pg_fetch_result',
+    'pg_fetch_row',
+    
+    //sqlite
+    'sqlite_fetch_all',
+    'sqlite_fetch_array',
+    'sqlite_fetch_object',
+    'sqlite_fetch_single',
+    'sqlite_fetch_string'
+);
+
+$DATABASE_WRITE_FUNCTIONS = array(
+    
+);
