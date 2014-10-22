b2cf9f3d93465b4ff1b66a5b65177f7336d8612e
Naomi Leow <naomileowwx@gmail.com>
2014-10-23 00:47:54 +0800

    with annotation rule for simplest form of variable assignment

--- lib/Phortress/Dephenses/Taint/StmtAnalyser.php
+++ lib/Phortress/Dephenses/Taint/StmtAnalyser.php
@@ -2,15 +2,36 @@
 namespace Phortress\Dephenses\Taint;
 
 /**
- * Most basic unit of the taint analyser. Takes in a statement and outputs 
+ *  Most basic unit of the taint analyser. Takes in a statement and outputs 
  * an array of variables modified by the statement, annotated based on a set 
  * of rules
- */
-/**
- * Description of newPHPClass
- *
  * @author naomileow
  */
 class StmtAnalyser {
-    //put your code here
+    private $statement;
+    
+    /**
+     * Array of variables modified by the statement
+     * @var AnnotVariable[]
+     */
+    private $annotations;
+    
+    public function __construct() {
+        
+    }
+    
+    public function reduce(){
+        
+    }
+    
+    private function applyBasicRule(\PhpParser\Node\Expr\AssignOp $assignOp){
+        $var = $assignOp->var;
+        $exp = $assignOp->expr;
+        
+        if($exp instanceof \PhpParser\Node\Scalar){
+            $annot = new Node\AnnotVariable($var->name, $var.getAttributes(), SAFE);
+        }elseif ($exp instanceof \PhpParser\Node\Variable) {
+            
+        }
+    }
 }
