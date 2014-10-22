365871573ea06ff464a906d824bdac3d92041c41
Naomi Leow <naomileowwx@gmail.com>
2014-10-23 00:48:39 +0800

    with rough sketch for function analyser

--- lib/Phortress/Dephenses/Taint/ClassAnalyser.php
+++ lib/Phortress/Dephenses/Taint/ClassAnalyser.php
@@ -1,16 +1,11 @@
 <?php
 namespace Phortress\Dephenses\Taint;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- * Description of LoopAnalyser
+ * Description of ClassAnalyser
  *
  * @author naomileow
  */
-class LoopAnalyser {
+class ClassAnalyser {
     //put your code here
 }
--- lib/Phortress/Dephenses/Taint/FunctionAnalyser.php
+++ lib/Phortress/Dephenses/Taint/FunctionAnalyser.php
@@ -1,10 +1,61 @@
 <?php
 namespace Phortress\Dephenses\Taint;
 /**
- * Description of newPHPClass
+ * This is a "template" for a defined function. Based on the Function object
+ * given at initialisation, an image of how the function's inputs affects its
+ * output and side effects will be created. 
  *
  * @author naomileow
  */
 class FunctionAnalyser {
-    //put your code here
+    
+    private $name;
+    
+    private $retByRef;
+    
+    private $statements;
+    
+    private $parameters;
+    
+    /**
+     * array(Return_Expression => 
+     *          array("variables" => 
+     *                      Names of variables the expression is dependent on 
+     *                "annotation" => precomputed annotations, if any)
+     */
+    private $outputs;
+    
+    
+    /**
+     * Array containing names of possibly vulnerable functions and the input 
+     * parameters they rely on
+     */
+    private $sideEffects;
+    
+    public function __construct($func) {
+        $this->initialiseFunctionAttributes($func);
+    }
+    
+    private function initialiseFunctionAttributes($func){
+        $this->name = $func->name;
+        $this->statements = $func->stmts;
+        $this->retByRef = $func->byRef;
+        $this->parameters = $func->params;
+        
+        $this->analyseFunctionParameters();
+        $this->analyseFunction();
+    }
+    
+    private function analyseFunctionParameters(){
+        
+    }
+    
+    private function analyseFunction(){
+        
+    }
+    
+    public function analyse($arguments, $variables){
+        
+    }
+    
 }
