d580323c0e40d63dcfacf8eb52c997d3cc45dadb
Joel Low <joel@joelsplace.sg>
2014-10-30 13:14:55 +0800

    Our include resolver, which is currently a no-op.

---
+++ lib/Phortress/IncludeResolver.php
@@ -0,0 +1,30 @@
+<?php
+namespace Phortress;
+
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+
+class IncludeResolver extends NodeVisitorAbstract
+{
+	/**
+	 * The files and the statements including them.
+	 * @var array(String => \PhpParser\Node[])
+	 */
+	private $files = array();
+
+	/**
+	 * Gets the mapping from files to statements.
+	 * 
+	 * @return array(string => Node[])
+	 */
+	public function getFiles() {
+		return $this->files;
+	}
+
+	public function enterNode(Node $node) {
+		if ($node instanceof Expr\Include_) {
+			assert(false, 'Includes not currently supported');
+		}
+	}
+} 
