b6c7d3d6a7536ff56f38523331f7de428319fab1
Joel Low <joel@joelsplace.sg>
2014-11-05 16:30:03 +0800

    Test defining functions in the global scope.

--- test/Phortress/EnvironmentTest.php
+++ test/Phortress/EnvironmentTest.php
@@ -3,6 +3,10 @@ namespace Phortress;
 
 class EnvironmentTest extends \PHPUnit_Framework_TestCase {
 	public function testCanFindFunction() {
+		$file = realpath(__DIR__ . '/Fixture/environment_test.php');
+		$program = loadGlassBoxProgram($file);
 
+		$this->assertTrue($program->environment->resolveFunction('a')
+			->environment->getParent() instanceof GlobalEnvironment);
 	}
 } 
