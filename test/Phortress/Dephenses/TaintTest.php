2b40f46d840f043143e6e02de5c972834bb8dcf4
Joel Low <joel@joelsplace.sg>
2014-11-13 08:38:35 +0800

    Failing test cases for taint analysis.

---
+++ test/Phortress/Dephenses/TaintTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace Phortress\Dephenses;
+
+use Phortress\Program;
+
+class TaintTest extends \PHPUnit_Framework_TestCase {
+	/**
+	 * The file we loaded the program from.
+	 *
+	 * @var String
+	 */
+	private $file;
+
+	/**
+	 * @var Program
+	 */
+	private $program;
+
+	public function setUp() {
+		// Load a program
+		$this->file = realpath(__DIR__ . '/../Fixture/taint_test.php');
+		$this->program = loadGlassBoxProgram($this->file);
+	}
+
+	public function testTaint() {
+		$taintDephense = new Taint();
+		$taintDephense->run($this->program->parseTree);
+	}
+}
