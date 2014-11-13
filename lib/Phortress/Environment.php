d836a67e37283f1065f83b93b9e12a3b29c0262d
Joel Low <joel@joelsplace.sg>
2014-11-13 08:36:48 +0800

    Better order for checking whether we should look into the parent environmen.

--- lib/Phortress/Environment.php
+++ lib/Phortress/Environment.php
@@ -189,8 +189,8 @@ abstract class Environment {
 
 		// We can only check our own local environment. We cannot pass a
 		// function environment and check our namespace for variables.
-		} else if ($this->shouldResolveVariablesInParentEnvironment() &&
-			!empty($this->getParent())) {
+		} else if (!empty($this->getParent()) &&
+			$this->shouldResolveVariablesInParentEnvironment()) {
 			return $this->getParent()->resolveVariable($variableName);
 		} else {
 			throw new UnboundIdentifierException($variableName, $this);
