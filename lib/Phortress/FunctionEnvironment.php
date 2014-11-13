5c7da0fb5a5c9c53df7201f740787ba6e2034073
Joel Low <joel@joelsplace.sg>
2014-11-13 16:14:29 +0800

    When constructing a function environment, specify the parent.

--- lib/Phortress/FunctionEnvironment.php
+++ lib/Phortress/FunctionEnvironment.php
@@ -2,6 +2,13 @@
 namespace Phortress;
 
 class FunctionEnvironment extends Environment {
+	public function __construct($name, Environment $parent) {
+		parent::__construct($name);
+		$this->parent = $parent;
+		self::copyValueReferences($parent->getGlobal()->getSuperglobals(),
+			$this->variables);
+	}
+
 	public function shouldResolveVariablesInParentEnvironment() {
 		return get_class($this->getParent()) === '\Phortress\FunctionEnvironment';
 	}
